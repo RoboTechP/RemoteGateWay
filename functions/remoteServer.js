64bab14aebdcfe53f678125d7ef57d737d31edc7
Arslan <arslanamird@gmail.com>
2024-07-21 17:59:53 +0200

    test

---
+++ functions/remoteServer.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const router = express.Router();
+
+app.use(bodyParser.json()); // Middleware to parse JSON bodies
+
+let receivedMessages = []; // Array to store received messages
+
+router.post('/api/receive', (req, res) => {
+    const newMessage = req.body;
+    receivedMessages.push(newMessage);
+
+    console.log('Received message object:', newMessage);
+
+    res.status(200).json({
+        message: 'Message received successfully',
+        data: newMessage
+    });
+});
+
+// API endpoint to fetch messages
+router.get('/api/messages', (req, res) => {
+    res.status(200).json(receivedMessages);
+});
+
+// Serve the frontend
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.use('/', router); // Use direct routes
+
+module.exports.handler = serverless(app);
