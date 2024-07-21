54225dfe71f7586d697f7a4ce5d136cbc3b0ed06
Arslan <arslanamird@gmail.com>
2024-07-21 17:43:59 +0200

    remote

--- server.js
+++ server.js
@@ -7,15 +7,35 @@ const router = express.Router();
 
 app.use(bodyParser.json()); // Middleware to parse JSON bodies
 
-router.post('/api/receive', (req, res) => {  // Changed to '/receive' to match your URL
-    const messageobj = req.body;
+let receivedMessages = []; // Array to store received messages
 
-    console.log('Received message object:', messageobj);
+router.post('/api/receive', (req, res) => {
+    const newMessage = req.body;
+    receivedMessages.push(newMessage);
+
+    console.log('Received message object:', newMessage);
 
     res.status(200).json({
         message: 'Message received successfully',
-        data: messageobj
+        data: newMessage
     });
+});
+
+// Serve the homepage
+router.get('/', (req, res) => {
+    const messagesHtml = receivedMessages.map(msg => `<pre>${JSON.stringify(msg, null, 2)}</pre>`).join('');
+
+    res.send(`
+        <html>
+            <head>
+                <title>Received Messages</title>
+            </head>
+            <body>
+                <h1>Received Messages</h1>
+                ${messagesHtml}
+            </body>
+        </html>
+    `);
 });
 
 app.use('/.netlify/functions/remoteServer', router);
